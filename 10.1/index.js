dd9556f282f2c19d7c895df70f57e648a428f353
jonathanalleman <jonathanalleman@gmail.com>
2024-05-16 18:05:57 -0500

    notes

--- 10.1/index.js
+++ 10.1/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 8082
 
+//notes
+
 app.use(express.json())
 app.use('/',(req, res)=>{
     res.send({status:200, message:"Hello World!"})
